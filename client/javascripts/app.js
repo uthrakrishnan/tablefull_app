cffe3e0bf50a6c14df870cbed91bf91ee277a569
Uthra Krishnan <uakrishnan@gmail.com>
2016-06-13 13:14:19 -0700

    added default configuration for

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -2,9 +2,31 @@
   angular
     .module('tableFull', ['ngRoute'])
     .config(function($authProvider){
+      $authProvider.httpInterceptor = function() { return true; },
+      $authProvider.withCredentials = true;
+      $authProvider.tokenRoot = null;
+      $authProvider.baseUrl = '/';
+      $authProvider.loginUrl = '/auth/login';
+      $authProvider.signupUrl = '/auth/signup';
+      $authProvider.unlinkUrl = '/auth/unlink/';
+      $authProvider.tokenName = 'token';
+      $authProvider.tokenPrefix = 'satellizer';
+      $authProvider.authHeader = 'Authorization';
+      $authProvider.authToken = 'Bearer';
+      $authProvider.storageType = 'localStorage';
       $authProvider.facebook({
         clientId: 'FACEBOOK APP ID',
-        responseType: 'token'
+        responseType: 'token',
+        name: 'facebook',
+        url: '/auth/facebook',
+        authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
+        redirectUri: window.location.origin + '/',
+        requiredUrlParams: ['display', 'scope'],
+        scope: ['email'],
+        scopeDelimiter: ',',
+        display: 'popup',
+        type: '2.0',
+        popupOptions: { width: 580, height: 400 }
       });
     })
 })
