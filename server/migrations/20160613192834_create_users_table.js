d6b12bfcd501fed98a4f458802c3eddfa32c42e0
Uthra Krishnan <uakrishnan@gmail.com>
2016-06-13 20:56:36 -0700

    added home layout

---
+++ server/migrations/20160613192834_create_users_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (t)=>{
+    t.increments();
+    t.text('username');
+    t.text('displayName');
+    t.text('profile_pic');
+    t.text('intro');
+    t.text('fb_id');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
